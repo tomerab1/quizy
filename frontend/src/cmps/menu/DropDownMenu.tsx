c9144f7547bbf33dda7de0ed31025b450c0bcd27
tomerab <tomer1452002@gmail.com>
2024-12-24 11:30:30 +0200

    Fixed border on bottom elem in menu

--- frontend/src/cmps/menu/DropDownMenu.tsx
+++ frontend/src/cmps/menu/DropDownMenu.tsx
@@ -44,7 +44,7 @@ export default function DropDownMenu({
           className={`absolute right-0 mr-5 mt-1 w-48 rounded-md bg-stone-600 py-1 shadow-lg ${isOpen ? "opacity-85" : ""}`}
         >
           {items.map((item) => (
-            <MenuItem className="mx-4 border-b border-stone-500 pb-2 pt-2 text-stone-100 hover:cursor-pointer hover:text-stone-400">
+            <MenuItem className="mx-2 border-b border-stone-500 pb-2 pl-3 pt-2 text-stone-100 last:border-0 hover:cursor-pointer hover:text-stone-400">
               <a href={item.href || "#"}>{item.label}</a>
             </MenuItem>
           ))}
